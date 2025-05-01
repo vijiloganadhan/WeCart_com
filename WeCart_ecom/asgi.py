0b3f5a914267dae0089977d23306c4bc3cc8e605
vijiloganadhan <mlvijayalakshmiloganadhan@gmail.com>
2025-05-01 13:34:47 +0530

    feat : lastest commit

--- WeCart_ecom/asgi.py
+++ WeCart_ecom/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for flipcart_ecom project.
+ASGI config for WeCart_ecom project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'flipcart_ecom.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WeCart_ecom.settings')
 
 application = get_asgi_application()
